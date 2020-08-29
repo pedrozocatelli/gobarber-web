0a027506868ea29833b89b926c6ef4d3d82b1d3f
Pedro Zocatelli <phz.pedro@gmail.com>
2020-08-29 14:26:23 -0300

    Creating repo, SignIn and SignUp pages

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Sign in to GoBarber</h1>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Password"
+        />
+
+        <Button type="submit">Sign in</Button>
+        <a href="forgot">Forgot password?</a>
+      </form>
+      <a href="login">
+        <FiLogIn />
+        Sign up
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: Record<string, unknown>): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Sign up to GoBarber</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Name" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+
+          <Button type="submit">Sign up for GoBarber</Button>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Already registered? Sign in
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
