01a9a9296a3ebcb94a98b748f5d85c63d756f262
Pedro Zocatelli <phz.pedro@gmail.com>
2021-03-09 13:37:51 -0300

    dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,70 +1,97 @@
-import React, { createContext, useCallback, useState, useContext } from 'react';
+import React, { useState, createContext, useContext, useCallback } from 'react';
+
 import api from '../services/api';
 
-interface AuthState {
-  token: string;
-  user: Record<string, unknown>;
+interface User {
+  avatar_url: string;
+  id: string;
+  name: string;
+  email: string;
 }
 
-interface SignInCredentialInterface {
+interface AuthContextInterface {
+  user: User;
+  signIn({ email, password }: SignInInterface): Promise<void>;
+  signOut(): void;
+  updateUser(data: User): void;
+}
+
+interface SignInInterface {
   email: string;
   password: string;
 }
 
-interface AuthContextInterface {
-  user: Record<string, unknown>;
-  signIn(credencials: SignInCredentialInterface): Promise<void>;
-  signOut(): void;
+interface AuthState {
+  token: string;
+  user: User;
 }
 
 const AuthContext = createContext<AuthContextInterface>(
   {} as AuthContextInterface,
 );
 
-export const AuthProvider: React.FC = ({ children }) => {
+const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
-    const token = localStorage.getItem('@GoBarber:token');
     const user = localStorage.getItem('@GoBarber:user');
+    const token = localStorage.getItem('@GoBarber:token');
 
-    if (token && user) {
-      return { token, user: JSON.parse(user) };
+    if (user && token) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      return {
+        token,
+        user: JSON.parse(user),
+      };
     }
     return {} as AuthState;
   });
 
-  const signIn = useCallback(async ({ email, password }) => {
-    const response = await api.post('sessions', {
-      email,
-      password,
-    });
+  const signIn = useCallback(async ({ email, password }: SignInInterface) => {
+    const response = await api.post('/sessions', { email, password });
+
     const { token, user } = response.data;
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
     setData({ token, user });
   }, []);
 
   const signOut = useCallback(() => {
-    localStorage.removeItem('@GoBarber:token');
     localStorage.removeItem('@GoBarber:user');
-
+    localStorage.removeItem('@GoBarber:token');
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (updatedData: User) => {
+      setData({
+        token: data.token,
+        user: updatedData,
+      });
+      localStorage.setItem('@GoBarber:user', JSON.stringify(updatedData));
+    },
+    [data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
 
-export function useAuth(): AuthContextInterface {
+function useAuth(): AuthContextInterface {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error('useAuth must used whitin an AuthProvider');
+    throw new Error('useAuth must be used within a AuthProvider');
   }
 
   return context;
 }
+
+export { AuthProvider, useAuth };
